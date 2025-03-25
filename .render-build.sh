70e61e5ba372bd34d064b55639768311d10e1269
K Mohammad <95924696+kattubadimohammad@users.noreply.github.com>
2025-03-25 13:54:41 +0530

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Install necessary dependencies
 apt-get update && apt-get install -y build-essential python3-dev gfortran
-pip install --upgrade pip setuptools
+
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools wheel
+
+# Install project requirements
 pip install -r requirements.txt
