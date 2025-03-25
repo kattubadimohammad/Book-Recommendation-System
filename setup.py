9b32809dcdfeb2766effe6b08ceea69452bfa41b
K Mohammad <95924696+kattubadimohammad@users.noreply.github.com>
2025-03-25 22:05:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+## Import Statements:
+from setuptools import setup
 
 ## Readme File Handling:
 with open("README.md", "r", encoding="utf-8") as f:
@@ -8,30 +9,21 @@ with open("README.md", "r", encoding="utf-8") as f:
 REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
 AUTHOR_USER_NAME = "K Mohammad"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit', 'numpy', 'pandas', 'scikit-learn']
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
 ## Setup Function:
 
 setup(
-    name=REPO_NAME,  # Changed to REPO_NAME for consistency
+    name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for Book Recommender System",  # Corrected description
+    description="A small package for Movie Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="miraclemohammad786@gmail.com",
-    packages=find_packages(),  # Use find_packages()
+    packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.7",
-    install_requires=LIST_OF_REQUIREMENTS,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    keywords="book recommendation system machine learning streamlit",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
+    install_requires=LIST_OF_REQUIREMENTS
 )
