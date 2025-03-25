11788f05dd9397ed84db18db6b4f85f4f38ca9b1
K Mohammad <95924696+kattubadimohammad@users.noreply.github.com>
2025-03-25 22:06:08 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-set -e
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Configure Streamlit
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 port = $PORT\n\
@@ -13,5 +7,3 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
-
-echo "Setup complete"
